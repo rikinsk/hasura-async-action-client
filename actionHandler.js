82d154fdbe6c7e87a66256b9e348e4d97239b050
rikinsk <rikin@hasura.io>
2023-02-14 22:38:44 +0530

    add actionHandler code

---
+++ actionHandler.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// paste the code from codegen here
+
+
+// Request Handler
+app.post('/sleepyAction', async (req, res) => {
+
+  // get request input
+  const { sleep } = req.body.input;
+
+  // run some business logic
+
+
+  console.log("Sleeping...");
+
+  await new Promise(resolve => setTimeout(resolve, sleep * 1000));
+
+  console.log("Slept for " + sleep + " seconds");
+
+  // success
+  return res.json({
+    msg: "Slept for " + sleep + " seconds",
+    status: "success"
+  })
+
+});
+
+
+app.get('/', async (req, res) => {
+  return res.json({
+    app: "running"
+  });
+});
+
+app.listen(PORT);
